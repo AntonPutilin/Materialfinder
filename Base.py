ed33cdba1f0d6d85cbb05efced49aad0a317252e
AntonPutilin <a_putilin@tut.by>
2025-07-10 12:53:57 +0300

    Update Base.py

--- Base.py
+++ Base.py
@@ -3,7 +3,7 @@ import streamlit as st
 # --- Настройки страницы ---
 st.set_page_config(
     page_title="Material finder",
-    layout="centered"
+    layout="wide"
 )
 
 # --- Список сайтов для поиска ---
